d4781279ba87f6c5015725065424e4634076a826
Martin Kurtsson <martin.kurtsson@daresay.co>
2020-03-25 13:24:10 +0100

    Tracking
    * Tracking
    * Verify every commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-console */
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`Using environment config: '${activeEnv}'`)
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: "Ring mera",
