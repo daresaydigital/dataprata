046c3c948e6d3fc92f376d2400b92cfe099f1d68
Mikael Weidenhielm <mikael.skoglund@daresay.co>
2020-03-25 17:10:28 +0100

    fixed test fail

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { useIntl, Link } from "gatsby-plugin-intl"
+import { useIntl } from "gatsby-plugin-intl"
 
 import Page from "../components/Page"
 import Container from "../components/Container"
