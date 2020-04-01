91de760edc5d040df47dba2d6e0300b7e011c9c1
Kim Fransman <kim.fransman@gmail.com>
2020-04-01 21:02:44 +0200

    Skype for iOS + Android as well

--- src/pages/skype.tsx
+++ src/pages/skype.tsx
@@ -19,6 +19,12 @@ import installImg from "../content/skypePage/skypeInstallScreenshot.png"
 import addContactsImg from "../content/skypePage/skypeAddContacts.png"
 import newContactsImg from "../content/skypePage/skypeNewContact.png"
 import makeACallImg from "../content/skypePage/skypeMakeACall.png"
+import iosAddContacts from "../content/skypePage/iosAddContacts.png"
+import androidAddContacts from "../content/skypePage/androidAddContacts.png"
+import iosStartVideoCall from "../content/skypePage/iosStartVideoCall.png"
+import androidMakeVideoCall from "../content/skypePage/androidMakeVideoCall.png"
+
+import downArrow from "../content/messengerPage/down.png"
 
 const cards = [
   { id: "toggleVideo", image: toggleVideoImg },
@@ -26,6 +32,34 @@ const cards = [
   { id: "cancelCall", image: cancelCallImg },
   { id: "openChat", image: openChatImg },
 ]
+
+const DownloadButton = styled.a`
+  text-align: left;
+  width: 100%;
+  background: #ffe000;
+  border-radius: 4px;
+  padding: 16px;
+  color: ${colors.black};
+  text-decoration: none;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  display: flex;
+  margin: 16px 0;
+  img {
+    align-self: flex-end;
+    width: 18px;
+    height: 18px;
+  }
+  span {
+    flex: 1;
+  }
+
+  &:hover {
+    background: #e0c500;
+  }
+`
 
 // TODO: This is now duplicated from service.tsx
 const Card = styled.div`
@@ -125,6 +159,34 @@ const SkypePage: React.FC = () => {
                   <TitleWithNumberCircle number={1}>{intl.formatMessage({ id: "skypepageInstall" })}</TitleWithNumberCircle>
                 </StyledDiv>
 
+                {os === "ios" && (
+                  <>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>Börja med att ladda ner Skype appen till din iPhone eller iPad.</Paragraph>
+                    </StyledDiv>
+                    <DownloadButton
+                      href="https://itunes.apple.com/app/apple-store/id304878510?pt=30244&ct=scom&mt=8"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span>Ladda ner Skype</span>
+                      <img src={downArrow} alt="Download Icon" />
+                    </DownloadButton>
+                  </>
+                )}
+
+                {os === "android" && (
+                  <>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>Börja med att ladda ner Skype appen till din Android.</Paragraph>
+                    </StyledDiv>
+                    <DownloadButton href="https://go.skype.com/skype.download.android" target="_blank" rel="noopener noreferrer">
+                      <span>Ladda ner Skype</span>
+                      <img src={downArrow} alt="Download Icon" />
+                    </DownloadButton>
+                  </>
+                )}
+
                 {os === "mac" && (
                   <>
                     <StyledDiv>
@@ -196,66 +258,133 @@ const SkypePage: React.FC = () => {
                 <div style={{ marginBottom: 16, marginTop: 16 }}>
                   <TitleWithNumberCircle number={2}>{intl.formatMessage({ id: "skypepageCreateAccount" })}</TitleWithNumberCircle>
                 </div>
-                <StyledDiv>
-                  <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph6" })}</Paragraph>
-                </StyledDiv>
-                <StyledDiv>
-                  <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph6b" })}</Paragraph>
-                </StyledDiv>
+
+                {(os === "mac" || os === "pc") && (
+                  <>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph6" })}</Paragraph>
+                    </StyledDiv>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph6b" })}</Paragraph>
+                    </StyledDiv>
+                  </>
+                )}
+
+                {(os === "ios" || os === "android") && (
+                  <>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>
+                        Om du inte har ett konto, öppna Skype och skapa ett konto med antingen ditt telefonnummer eller din mailadress
+                      </Paragraph>
+                    </StyledDiv>
+                  </>
+                )}
 
                 <div style={{ marginBottom: 16, marginTop: 16 }}>
                   <TitleWithNumberCircle number={3}>{intl.formatMessage({ id: "skypepageAddContacts" })}</TitleWithNumberCircle>
                 </div>
 
-                <StyledDiv>
-                  <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph7" })}</Paragraph>
-                </StyledDiv>
+                {(os === "mac" || os === "pc") && (
+                  <>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph7" })}</Paragraph>
+                    </StyledDiv>
 
-                <StyledDiv>
-                  <img src={addContactsImg} alt="Screenshot of adding contacts" />
-                </StyledDiv>
+                    <StyledDiv>
+                      <img src={addContactsImg} alt="Screenshot of adding contacts" />
+                    </StyledDiv>
 
-                <StyledDiv>
-                  <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph8" })}</Paragraph>
-                </StyledDiv>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph8" })}</Paragraph>
+                    </StyledDiv>
 
-                <StyledDiv>
-                  <img src={newContactsImg} alt="Screenshot of adding contacts" />
-                </StyledDiv>
+                    <StyledDiv>
+                      <img src={newContactsImg} alt="Screenshot of adding contacts" />
+                    </StyledDiv>
+                  </>
+                )}
+
+                {(os === "ios" || os === "android") && (
+                  <>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>
+                        För att lägga till kontakter, klicka på “Kontakter”-ikonen längst ner till höger för att komma till dina kontakter.
+                      </Paragraph>
+                    </StyledDiv>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>
+                        Klicka sedan på Ikonen nere i högra hörnet och sök efter den du vill ringa antingen via personens Skype namn,
+                        mailadress eller telefonnummer.
+                      </Paragraph>
+                    </StyledDiv>
+                    <StyledDiv>
+                      {os === "ios" && <img src={iosAddContacts} alt="Screenshot of adding contacts" />}
+                      {os === "android" && <img src={androidAddContacts} alt="Screenshot of adding contacts" />}
+                    </StyledDiv>
+                  </>
+                )}
 
                 <div style={{ marginBottom: 16, marginTop: 16 }}>
                   <TitleWithNumberCircle number={4}>{intl.formatMessage({ id: "skypepageMakeAVideoCall" })}</TitleWithNumberCircle>
                 </div>
 
-                <StyledDiv>
-                  <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph9" })}</Paragraph>
-                </StyledDiv>
+                {(os === "mac" || os === "pc") && (
+                  <>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph9" })}</Paragraph>
+                    </StyledDiv>
 
-                <StyledDiv>
-                  <img src={makeACallImg} alt="Screenshot of adding contacts" />
-                </StyledDiv>
+                    <StyledDiv>
+                      <img src={makeACallImg} alt="Screenshot of making a call" />
+                    </StyledDiv>
+                  </>
+                )}
+
+                {(os === "ios" || os === "android") && (
+                  <>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>
+                        Från kontakter, välj den du vill ta kontakt med och klicka på personens namn.
+                      </Paragraph>
+                    </StyledDiv>
+                    <StyledDiv>
+                      <Paragraph color={colors.gray.dark}>
+                        Efter du klickat på den du vill videochatta med, tryck på videokameran uppe i högra hörnet för att starta samtalet.
+                      </Paragraph>
+                    </StyledDiv>
+
+                    <StyledDiv>
+                      {os === "ios" && <img src={iosStartVideoCall} alt="Screenshot of making a call" />}
+                      {os === "android" && <img src={androidMakeVideoCall} alt="Screenshot of making a call" />}
+                    </StyledDiv>
+                  </>
+                )}
 
                 <div style={{ marginBottom: 16, marginTop: 16 }}>
                   <TitleWithNumberCircle number={5}>{intl.formatMessage({ id: "skypeDuringTheCall" })}</TitleWithNumberCircle>
                 </div>
-
                 <StyledDiv>
                   <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: "skypepageParagraph10" })}</Paragraph>
                 </StyledDiv>
 
-                {cards.map((card) => (
-                  <Card key={card.id}>
-                    <div className="cardHeader">
-                      <div style={{ marginRight: 16 }}>
-                        <img src={card.image} alt={`Skype ${card.id} icon`} />
+                {cards.map((card) => {
+                  if ((os === "ios" || os === "android") && card.id === "openChat") {
+                    return null
+                  }
+                  return (
+                    <Card key={card.id}>
+                      <div className="cardHeader">
+                        <div style={{ marginRight: 16 }}>
+                          <img src={card.image} alt={`Skype ${card.id} icon`} />
+                        </div>
+                        <Header1>{intl.formatMessage({ id: `skype${card.id}Title` })}</Header1>
                       </div>
-                      <Header1>{intl.formatMessage({ id: `skype${card.id}Title` })}</Header1>
-                    </div>
-                    <StyledDiv>
-                      <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: `skype${card.id}Info` })}</Paragraph>
-                    </StyledDiv>
-                  </Card>
-                ))}
+                      <StyledDiv>
+                        <Paragraph color={colors.gray.dark}>{intl.formatMessage({ id: `skype${card.id}Info` })}</Paragraph>
+                      </StyledDiv>
+                    </Card>
+                  )
+                })}
               </>
             )}
           </AnalyticsContext.Consumer>
